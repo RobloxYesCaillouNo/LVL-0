58bb483fc02aa5d84e1da443b467c8945e85e0ae
Love4653 <Kaiya.park@gmail.com>
2016-06-12 11:27:50 -0700

    sta
    ΩΩ≈≈ççç√√∫˜˜µ≤≥¬∆∆∆©∂åßƒ˙∆∆˚˚¬¬…………∆˙©©©˙™™£¢∞∞¶∞

---
+++ src/HoroScope.java
@@ -0,0 +1,87 @@
+
+import javax.swing.JOptionPane;
+
+public class HoroScope {
+public static void main(String[] args) {
+	
+String Input = JOptionPane.showInputDialog("What's your zodiac sign?");
+
+if(Input.equals("Cancer")){
+JOptionPane.showMessageDialog(null,"Cancer Astrology June 21 - July 22");
+
+
+
+else if(Input.equals(""))
+JOptionPane.showMessageDialog(null,"boooooooooooo");
+
+else if(Input.equals(""))
+JOptionPane.showMessageDialog(null,"boooooooooooo");
+else if(Input.equals(""))
+JOptionPane.showMessageDialog(null,"boooooooooooo");
+else if(Input.equals(""))
+	JOptionPane.showMessageDialog(null,"boooooooooooo");
+else if(Input.equals(""))
+JOptionPane.showMessageDialog(null,"boooooooooooo");
+else if(Input.equals(""))
+JOptionPane.showMessageDialog(null,"boooooooooooo");
+else if(Input.equals(""))
+JOptionPane.showMessageDialog(null,"boooooooooooo");
+else if(Input.equals(""))
+JOptionPane.showMessageDialog(null,"boooooooooooo");
+else if(Input.equals(""))
+JOptionPane.showMessageDialog(null,"boooooooooooo");
+else if(Input.equals(""))
+JOptionPane.showMessageDialog(null,"boooooooooooo");
+else if(Input.equals(""))
+JOptionPane.showMessageDialog(null,"boooooooooooo");
+else if(Input.equals(""))
+JOptionPane.showMessageDialog(null,"boooooooooooo");
+else if(Input.equals(""))
+JOptionPane.showMessageDialog(null,"boooooooooooo");
+
+
+
+
+
+
+
+
+
+
+	}
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ src/SecretMessageBox.java
@@ -0,0 +1,35 @@
+import javax.swing.JOptionPane;
+
+/**
+ * Secret Message Box / Secure Messaging System
+ * 
+ * You want to leave a message on one of the Mac computers so that only your friend can read it.
+ * You set up the passcode and the secret message. 
+ * Your friend types in the passcode to retrieve the message.
+ * 
+ */
+
+public class SecretMessageBox {
+
+	// 0. Make a main method and put steps 1-5 inside it
+	public static void main(String[] args) {
+		
+	
+	// 1. Set the passcode in a String variable
+	String passcode = "78324";
+	// 2. Using a pop-up, ask for a secret message and store it in a variable
+	String poop = JOptionPane.showInputDialog("Want to know the secret Message??");
+	// 3. Ask your friend for the passcode and store it in a variable
+	String guest = JOptionPane.showInputDialog("passcode please");
+	// 4. If the passcode matches, show the secret message
+	if (guest.equals("78324") ) {
+	JOptionPane.showMessageDialog(null, poop);	
+	}
+	// 5. If the passcode does not match, pop-up "INTRUDER!!"
+	else {
+	JOptionPane.showMessageDialog(null,"INTRUDER!!!!!!!!!!!!!");
+	}
+	}
+}
+
+
