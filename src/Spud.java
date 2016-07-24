221071715a4dd7ac7b9c651bf12e23532814f970
Jake Mitchell <legomanaz@gmail.com>
2016-07-23 17:04:29 -0700

    EpicFac

---
+++ src/Spud.java
@@ -0,0 +1,35 @@
+import org.omg.Messaging.SyncScopeHelper;
+
+public class Spud {
+public static void main(String[] args) {
+	
+	
+	for (int i = 1; i < 4; i++) {
+		
+		System.out.println(i+" Potato");
+		}
+	System.out.println("Four");
+	
+	for (int i = 5; i < 8; i++) {
+	System.out.println(i+" Potato");
+	}
+	System.out.println("More");
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+}
+}
