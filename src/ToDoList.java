b49ecae29761934e92d0eb95dcd36d3673d8a659
Leauge <dont fill this in! />
2016-08-20 17:25:15 -0700

    GotMIlk?
     ¬_¬

---
+++ src/ToDoList.java
@@ -0,0 +1,9 @@
+import javax.swing.JOptionPane;
+
+public class ToDoList {
+public static void main(String[] args) {
+String task = JOptionPane.showInputDialog("Enter a task");
+	JOptionPane.showMessageDialog(null,task+"\n and do a barrel roll");
+	
+}
+}
