d555afe1956e713222b8d7a3f5b6b8929d8360d1
QUAGMIRE <league@WTS11.attlocal.net>
2016-10-08 17:29:45 -0700

    RatsNestBoi
    :-)

---
+++ src/BADGERBADGERBADGER.java
@@ -0,0 +1,40 @@
+
+public class BADGERBADGERBADGER {
+public static void main(String[] args) {
+	
+	for (int i = 0; i < 100000000; i++) {
+		
+	
+	for (int e = 0; e < 11; e++) {
+		System.out.println("badger");
+	}
+	
+	for (int r = 0; r < 2; r++) {
+		System.out.println("Mushroom");
+	}
+	
+	for (int b = 0; b < 10; b++) {
+		System.out.println("Badger");
+	}
+	
+	for (int l = 0; l < 2; l++) {
+		System.out.println("snakes");
+	}
+	
+
+	}
+	
+	
+	
+	 }
+	
+	
+	
+	
+	
+	
+	
+	
+	
+}
+
---
+++ src/BEERdueds.java
@@ -0,0 +1,15 @@
+
+public class BEERdueds {
+public static void main(String[] args) {
+	while(true){
+	for (int i = 99; i > 0; i--) {
+	
+		System.out.println(i+" bottles of cokane on the wall. "+i+" bottles of cokane. Take one down. Pass it around. "+ (i-1)+" bottles of cokane on the wall" );
+	if(i == 0){
+		System.out.println("Then we restock...");
+	}
+	}
+	}
+	
+	}
+}
