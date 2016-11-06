8c7f190aa643cea611ec4ad53a869434624a4f1c
DJA526 <league@WTS13.attlocal.net>
2016-11-05 17:29:24 -0700

    uaisldjkfhaboilsbvodluhjdsafasdhadsgasdf
    made buy 983y4

---
+++ src/tonguetwister.java
@@ -0,0 +1,17 @@
+import java.util.Random;
+
+import javax.swing.JOptionPane;
+
+public class tonguetwister {
+public static void main(String[] args) {
+	
+		 JOptionPane.showConfirmDialog(null,"Can you can a can as a canncer can can a can?");
+
+	int rand=new Random().nextInt(1000);
+	for (int i = 0; i <= rand; i++) {
+		System.out.println("Can you can a can as a canncer can can a can?");
+
+	}
+	
+}
+}
