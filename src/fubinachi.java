461a7e3ea5e4016ae39fef65c2dd597fa0a580e8
DJA526 <league@WTS13.attlocal.net>
2016-12-10 17:27:41 -0800

    (╯°□°）╯︵ ┻━┻

--- src/fubinachi.java
+++ src/fubinachi.java
@@ -6,11 +6,10 @@ public class fubinachi {
 		int A = 0;
 		int B = 1;
 		int C = A+B;
-		int D = B+C;
-		int E=F;
-		int F=G;
-		for (int i = 0; i < 100000000; i++) {
-			System.out.println(A,B,C,D);
+		int 0=1
+		int 1=2;
+		for (int i = 0; i < 12; i++) {
+			System.out.println("Caci");
 		}
 
 	}
