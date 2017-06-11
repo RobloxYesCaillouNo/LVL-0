136f4ceb80cbee589602eb316ee194a2f8a026ab
RobloxYesCaillouNo <league@WorthyMini-6.attlocal.net>
2017-06-10 17:00:57 -0700

    Roblox isn't caner noobs vs dong!
    sdi0ort guipfdxhgiozsdfhgsuihdf[i0 g lol look

---
+++ src/qazsedcftDDD.java
@@ -0,0 +1,20 @@
+import javax.swing.JOptionPane;
+
+public class qazsedcftDDD {
+	public static void main(String[] args, int i) {
+		String Dot = JOptionPane.showInputDialog("Insert number here or play on my tacky roblox game!");
+		int Robloxisbetter = Integer.parseInt(Dot);
+
+		for (int j = 2; j < 1000000; j++) {
+			if (Robloxisbetter % 2 == 2) {
+				JOptionPane.showMessageDialog(null, Dot + " is not a Prime number");
+			}
+		}
+
+		for (int i1 = 3; i1 < 1000000; i1++) {
+		}
+		if (Robloxisbetter % 3 == i) {
+			JOptionPane.showMessageDialog(null, Dot + " is a Prime Number");
+		}
+	}
+}
