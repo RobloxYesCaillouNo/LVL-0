7bd6dbed664e2ae90b08cbd3f46717ff018fa276
CatcusCat <league@WTS15.attlocal.net>
2017-06-17 17:42:49 -0700

    whisperingacne444666

---
+++ src/contentDeleted.java
@@ -0,0 +1,6 @@
+
+public class contentDeleted {
+	public static void main(String[] args) {
+		
+	}
+}
---
+++ src/usdg.java
@@ -0,0 +1,13 @@
+
+class Upper {
+	public static void main(String[] args) {
+	String rab = MakeUpperC4se("hello");	
+	System.out.println(rab);
+	}
+
+	public static String MakeUpperC4se(String str) {
+	return	str.toUpperCase();
+	
+	}	
+	
+}
