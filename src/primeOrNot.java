2dc0c496326faa1933474cd5b6ca7ff3d53a5a25
RobloxYesCaillouNo <league@WTS-IM-17.attlocal.net>
2017-09-23 16:01:07 -0700

    starry night

---
+++ src/primeOrNot.java
@@ -0,0 +1,9 @@
+import javax.swing.JOptionPane;
+
+public class primeOrNot {
+	public static void main(String[] args) {
+		String g = JOptionPane.showInputDialog("Insert a number to see if it is prime or not");
+		int b = Integer.parseInt(g);
+
+	}
+}
